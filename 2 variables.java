132201054e503c3b5e6d72f5111d0772599d81b4
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-15 13:13:09 +0530

    Create 2 variables.java

---
+++ 2 variables.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+        // Variable
+        String Myname = "Tanuka";
+        System.out.println(Myname);
+        System.out.println("My name is " + Myname);
+        int age = 21;
+        System.out.println("My age is " + age);
+        String name = Myname;
+        System.out.println(name);
+    }
+}
+
+/*
+Tanuka
+My name is Tanuka
+My age is 21
+Tanuka
+*/
