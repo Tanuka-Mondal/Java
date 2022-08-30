88a6d6c3bec1a82f6bfdfdeafc6446bdb933fe68
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-31 00:48:01 +0530

    Create 8_final.java

---
+++ 8_final.java
@@ -0,0 +1,8 @@
+public class myconstant {
+    public static void main(String[] args){
+        final float PI = 3.14f;  // final is used to declare a constant variable, which cannot be changed.
+        System.out.println(PI);
+    }
+}
+
+// 3.14
