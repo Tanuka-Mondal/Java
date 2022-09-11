29fb586066e89b148b5dd69e0f8049c50dec8773
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-11 23:54:08 +0530

    Create 10_math_class.java

---
+++ 10_math_class.java
@@ -0,0 +1,15 @@
+public class mathsclass {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 31;
+        System.out.println(Math.max(a,b)); //31
+        System.out.println(Math.min(a,b)); //25
+        System.out.println(Math.sqrt(a)); //5.0
+        int c = -6;
+        System.out.println(Math.abs(c)); //6
+        int d = 2;
+        System.out.println(Math.pow(a,d)); //625.0
+        System.out.println(Math.random()); //0.0 to 1.0
+        System.out.println((int)(Math.random()*100)); //0 to 99
+    }
+}
