ae0a3a838695ee52613f092594228e01ff6ffc45
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-12 01:24:45 +0530

    Create 12_conditional_statement.java

---
+++ 12_conditional_statement.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+       
+        int age = 20;
+        if (age > 18) {
+            System.out.println("You are eligible to vote");
+        } else {
+            System.out.println("You are not eligible to vote");
+        }
+    }
+}
